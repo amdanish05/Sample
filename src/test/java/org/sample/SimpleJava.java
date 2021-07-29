0cfe8037a1ad11c8ebf402244debba67c1e404b0
amdanish05 <amdanish.bca@gmail.com>
2021-07-30 00:58:28 +0530

    Sample

---
+++ src/test/java/org/sample/SimpleJava.java
@@ -0,0 +1,13 @@
+package org.sample;
+
+public class SimpleJava {
+	private void tc0() {
+		System.out.println("tc0");
+
+	}
+	public static void main(String[] args) {
+		SimpleJava j=new SimpleJava();
+		j.tc0();
+		
+	}
+}
